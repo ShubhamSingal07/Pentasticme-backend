3bdb987ee9e4897c7549e0ff508af91c0a5e37d1
Shubham Singal <singalshubham312@gmail.com>
2019-08-20 14:09:56 +0530

    changed projection in user Controller

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,7 @@ const { User } = require("../models/user");
 
 const getUser = async id => {
   try {
-    const user = await User.findById(id, { username: 1, thumbnail: 1, role: 1 });
+    const user = await User.findById(id, { username: 1, thumbnail: 1, role: 1, strategy: 1 });
     return user;
   } catch (err) {
     throw new Error("Could not connect to Database. Please try again later");
