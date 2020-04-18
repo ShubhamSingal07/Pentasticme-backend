9259399002b7de6be37a3487e610b756d267521d
Shubham Singal <singalshubham312@gmail.com>
2020-04-18 12:14:06 +0530

    config.dev.js

---
+++ config.dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  MONGODB_URL: "",
+  JWT_SECRET: "",
+  CLOUDINARY_CLOUD_NAME: "",
+  CLOUDINARY_API_SECRET: "",
+  CLOUDINARY_API_KEY: "",
+  EMAIL_ID: "",
+  EMAIL_PASS: "",
+};
